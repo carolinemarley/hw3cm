dc261ee775061bc28c011f699805e24971089c38
carolinemarley <143441565+carolinemarley@users.noreply.github.com>
2024-10-02 11:22:08 -0500

    Update model-authors.php

--- model-authors.php
+++ model-authors.php
@@ -1,6 +1,19 @@
 <?php
 
-
+function selectAuthors() {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("SELECT AuthorID, FirstName, LastName FROM [dbo].[Author]");
+        $stmt->bind_param("s", $uEmail);
+        $stmt->execute();
+        $result = $stmt->get_result();
+        $conn->close();
+        return $result;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
 
 
 ?>
