c2bc36cf499eccc4bb46b11dc54953396757c609
carolinemarley <143441565+carolinemarley@users.noreply.github.com>
2024-10-02 11:10:37 -0500

    Create authors.php

---
+++ authors.php
@@ -0,0 +1,10 @@
+<?php
+require_once ("util-db.php");
+require_once("model-authors.php");
+
+$pageTitle = "Authors";
+include "view-header.php";
+$authors = selectAuthors(); 
+include "view-authors.php";
+include "view-footer";
+?>
